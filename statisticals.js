3e5d8f609928ef4243c91ede27a8dd860b8a46ff
StvanLeo <estebanoleas@gmail.com>
2025-06-12 01:13:43 -0500

    Arreglar los modulos exportados de estadistica

--- statisticals.js
+++ statisticals.js
@@ -2,10 +2,10 @@ function average(arr) {
   return arr.reduce((a, b) => a + b, 0) / arr.length;
 }
 
-function standarDeviation(arr) {
+function standardDeviation(arr) {
   const media = average(arr);
   const sumSquares = arr.reduce((acc, val) => acc + (val - media) ** 2, 0);
   return Math.sqrt(sumSquares / arr.length);
 }
 
-export default { average, standarDeviation };
+module.exports = { average, standardDeviation };
